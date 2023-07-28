57455acc4f0a197317465bbfc88e2bb38fa4569e
Philmg <phillipmguetterman@att.net>
2023-07-27 21:38:07 -0500

    add and format parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,4 +37,13 @@ fetch(showtimesURL, {
 
 searchButton.addEventListener("click", () => {
 
-})
+});
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
