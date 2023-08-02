dfd08b66afc9633367b0209782b6ae6548c62537
btsmith1212 <135077506+btsmith1212@users.noreply.github.com>
2023-08-01 21:20:09 -0500

    first api functionality complete, working on second

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,28 @@ var searchResults = document.querySelector('#searchResults');
 let resultsCard = document.querySelector('#resultsCard');
 let rangeNumber = document.querySelector('#myRange');
 let ratingSelected = document.querySelector('#ratingSelected');
+let movieCheckbox = document.querySelector('#movieType');
+let seriesCheckbox = document.querySelector('#showType');
+
+function getType(){
+	if (movieCheckbox.checked && seriesCheckbox.checked){
+		return "all"
+	} else if (movieCheckbox.checked){
+		return "movie"
+	} else if (seriesCheckbox.checked){
+		return "series"
+	} else {
+		return "all"
+	}
+}
 
 
 searchButton.addEventListener('click', () => {
-	const streamAPI = 'https://streaming-availability.p.rapidapi.com/v2/search/title?title=batman&country=us&show_type=movie&output_language=en';
+	getType();
+	console.log(getType());
+	let searchedMovie = searchBar.value;
+	const streamAPI = `https://streaming-availability.p.rapidapi.com/v2/search/title?title=${searchedMovie}&country=us&show_type=${getType()}&output_language=en`;
+    // title, shotype
 	fetch(streamAPI, {
 		method: 'GET',
 		headers: {
@@ -44,52 +62,51 @@ searchButton.addEventListener('click', () => {
 	})
 });
 
-// searchButton.addEventListener('click', () => {
-// 	let searchedMovie = searchBar.value;
-// 	var showtimesURL = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}/`;
-// 	fetch(showtimesURL, {
-// 		headers: {
-// 			Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmRlNGViNTc2ZTQxNjY2NzIxZjAxYTcxNGRkNDkwOCIsInN1YiI6IjY0Yzg0NGQxYzA0OGE5MDExY2Q3ZmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gi21mwDNoFGGVyoJeEh7iCgHVuDxTYUNIMzvIDak_7I"
-// 		}
-// 	}).then(function (response) {
-// 		console.log(response)
-// 		return response.json();
-// 	}).then(function (data) {
-// 		console.log(data);
-// 		nowShowingDiv.innerHTML = '';
-// 		for (var i = 0; i < data.results.length; i++) {
-// 			console.log(data.results.length)
-// 			movieCard = document.createElement('p');
-// 			movieCard.innerHTML =
-// 				`<div id="movieCard">
-// 			<div id="movieImage">
+searchButton.addEventListener('click', () => {
+	let searchedMovie = searchBar.value;
+	var showtimesURL = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}&vote_average.gte=10&overview=/`;
+	fetch(showtimesURL, {
+		headers: {
+			Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmRlNGViNTc2ZTQxNjY2NzIxZjAxYTcxNGRkNDkwOCIsInN1YiI6IjY0Yzg0NGQxYzA0OGE5MDExY2Q3ZmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gi21mwDNoFGGVyoJeEh7iCgHVuDxTYUNIMzvIDak_7I"
+		}
+	}).then(function (response) {
+		console.log(response)
+		return response.json();
+	}).then(function (data) {
+		console.log(data);
+		nowShowingDiv.innerHTML = '';
+		for (var i = 0; i < data.results.length; i++) {
+			console.log(data.results.length)
+			movieCard = document.createElement('p');
+			movieCard.innerHTML =
+			`<div id="movieCard">
+			    <div id="movieImage">
+			</div>
+			<div id="cardTop">
+				<h2>${data.results[i].title}</h2>
+				<h2>${data.results[i].vote_average}</h2>
+			</div>
+			<div id="cardBot">
+				<h3>${data.results[i].overview}</h3>
+			</div>
+		</div>`
 
-// 			</div>
-// 			<div id="cardTop">
-// 				<h2>${data.results[i].title}</h2>
-// 				<h2>${data.results[i].vote_average}</h2>
-// 			</div>
-// 			<div id="cardBot">
-// 				<h3>${data.results[i].overview}</h3>
-// 			</div>
-// 		</div>`
-
-// 			nowShowingDiv.appendChild(movieCard);
-// 		}
-// 	})
-// })
+			nowShowingDiv.appendChild(movieCard);
+		}
+	})
+})
 
 
-// for query need 'status', 'original_title', 'genres:array'
+// for query need title, overview, rating
 searchButton.addEventListener("click", () => {
 
 });
 
-// var slider = document.getElementById("myRange");
-// var output = document.getElementById("demo");
-// output.innerHTML = slider.value; // Display the default slider value
+searchBar.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+        searchButton.click();
+    }
+});
 
-// // Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function () {
-//     output.innerHTML = this.value;
-// }
+
+// Update the current slider value (each time you drag the slider handle)
