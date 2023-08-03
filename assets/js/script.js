7f5ae9597074c6ffc29224e7441faef06e2c9aa0
ahmell <andrewmell10@gmail.com>
2023-08-03 10:26:21 -0500

    fixed a bug that prevented the release year parameter from functioning correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@ let savedSearches = document.querySelector('#history-buttons');
 var searchButtonRapid = document.querySelector('#searchbtnRapid');
 var searchBarRapid = document.querySelector('#searchBarRapid');
 let releaseYear = document.getElementById("releaseYear");
-let releaseYearValue = releaseYear.value
 
 
 renderButtons();
@@ -101,43 +100,43 @@ function getNowStreaming(){
 };
 
 
-function getNowPlaying(searchedMovie){
-	var showtimesURL = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}&vote_average.gte=10&overview=/`;
-	fetch(showtimesURL, {
-		headers: {
-			Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmRlNGViNTc2ZTQxNjY2NzIxZjAxYTcxNGRkNDkwOCIsInN1YiI6IjY0Yzg0NGQxYzA0OGE5MDExY2Q3ZmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gi21mwDNoFGGVyoJeEh7iCgHVuDxTYUNIMzvIDak_7I"
-		}
-	}).then(function (response) {
-		console.log(response)
-		return response.json();
-	}).then(function (data) {
-		console.log(data);
-		saveSearch(data.results[0].title);
-		nowShowingDiv.innerHTML = '';
-		for (var i = 0; i < data.results.length; i++) {
-			console.log(data.results.length)
-			if (data.results[i].original_language !== "en") continue;
-			movieCard = document.createElement('p');
-			movieCard.innerHTML =
+// function getNowPlaying(searchedMovie){
+// 	var showtimesURL = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}&vote_average.gte=10&overview=/`;
+// 	fetch(showtimesURL, {
+// 		headers: {
+// 			Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmRlNGViNTc2ZTQxNjY2NzIxZjAxYTcxNGRkNDkwOCIsInN1YiI6IjY0Yzg0NGQxYzA0OGE5MDExY2Q3ZmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gi21mwDNoFGGVyoJeEh7iCgHVuDxTYUNIMzvIDak_7I"
+// 		}
+// 	}).then(function (response) {
+// 		console.log(response)
+// 		return response.json();
+// 	}).then(function (data) {
+// 		console.log(data);
+// 		saveSearch(data.results[0].title);
+// 		nowShowingDiv.innerHTML = '';
+// 		for (var i = 0; i < data.results.length; i++) {
+// 			console.log(data.results.length)
+// 			if (data.results[i].original_language !== "en") continue;
+// 			movieCard = document.createElement('p');
+// 			movieCard.innerHTML =
 
-			`<div id="movieCard" class="border-2">
+// 			`<div id="movieCard" class="border-2">
 
-			    <div id="movieImage">
-				<img src="https://image.tmdb.org/t/p/original/${data.results[i].poster_path}" class="object-scale-down h-48 w-96"/>
-			</div>
-			<div id="cardTop">
-				<h2>${data.results[i].title}</h2>
-				<h2>${data.results[i].vote_average}</h2>
-			</div>
-			<div id="cardBot">
-				<h3>${data.results[i].overview}</h3>
-			</div>
-		</div>`
+// 			    <div id="movieImage">
+// 				<img src="https://image.tmdb.org/t/p/original/${data.results[i].poster_path}" class="object-scale-down h-48 w-96"/>
+// 			</div>
+// 			<div id="cardTop">
+// 				<h2>${data.results[i].title}</h2>
+// 				<h2>${data.results[i].vote_average}</h2>
+// 			</div>
+// 			<div id="cardBot">
+// 				<h3>${data.results[i].overview}</h3>
+// 			</div>
+// 		</div>`
 
-			nowShowingDiv.appendChild(movieCard);
-		}
-	})
-}
+// 			nowShowingDiv.appendChild(movieCard);
+// 		}
+// 	})
+// }
 
 function renderButtons() {
     savedSearches.innerHTML = '';
@@ -153,7 +152,8 @@ function renderButtons() {
     }}
 }
 function getNowPlaying(searchedMovie){
-	var showtimesURL = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}&vote_average.gte=10&primary_release_year=${releaseYearValue}&overview=/`;
+	let releaseYearValue = releaseYear.value
+	var showtimesURL = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}&vote_average.gte=7&primary_release_year=${releaseYearValue}&overview=/`;
 	fetch(showtimesURL, {
 		headers: {
 			Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmRlNGViNTc2ZTQxNjY2NzIxZjAxYTcxNGRkNDkwOCIsInN1YiI6IjY0Yzg0NGQxYzA0OGE5MDExY2Q3ZmZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gi21mwDNoFGGVyoJeEh7iCgHVuDxTYUNIMzvIDak_7I"
@@ -214,7 +214,7 @@ function getStreaming(streamingInfo){
 }
 
 searchButton.addEventListener('click', () => {
-	console.log(releaseYearValue);
+	// console.log(releaseYearValue);
 	getNowPlaying(searchBar.value);
 })
 
